ae561a40c27982e716a139fe0ede791afd9570c6
ao627515 <ao627515@gmail.com>
2024-07-08 23:29:59 +0000

    mj

--- Gemfile
+++ Gemfile
@@ -1,13 +1,7 @@
 source "https://rubygems.org"
-
 ruby '3.3.3'
-
 gem 'rspec'
-
 gem 'pry'
-
 gem 'rubocop', require: false
-
 gem 'dotenv'
-
-gem 'twitter'
+gem "x", "~> 0.14.1"
--- lib/app.rb
+++ lib/app.rb
@@ -1,18 +1,31 @@
 require 'pry'
-require 'twitter'
+require 'x'
 require 'dotenv'
 Dotenv.load('.env') 
 
-client = Twitter::REST::Client.new do |config|
+x_credentials = {
+  api_key:             ENV['X_API_KEY'],
+  api_key_secret:      ENV['X_API_KEY_SECRET'],
+  access_token:        ENV['X_ACCESS_TOKEN'],
+  access_token_secret: ENV['X_ACCESS_TOKEN_SECRET'],
+}
 
-    config.consumer_key        = ENV["TWITTER_CONSUMER_KEY"]
+# Initialize an API v1.1 client
+v1_client = X::Client.new(base_url: "https://api.twitter.com/1.1/", **x_credentials)
+puts v1_client.get('users/show.json').inspect
 
-    config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"]
+# # Define a custom response object
+# Language = Struct.new(:code, :name, :local_name, :status, :debug)
 
-    config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
+# # Parse a response with custom array and object classes
+# languages = v1_client.get("help/languages.json", object_class: Language, array_class: Set)
+# # <Set: {#<struct Language code="ur", name="Urdu", local_name="اردو", status="beta", debug=false>, …
 
-    config.access_token_secret = ENV["TWITTER_ACCESS_TOKEN_SECRET"]
+# # Access data with dots instead of brackets
+# languages.first.local_name.inspect
 
- end
+# # Initialize an Ads API client
+# ads_client = X::Client.new(base_url: "https://ads-api.twitter.com/12/", **x_credentials)
 
- client.update('Mon premier tweet en Ruby !!!!')
+# # Get your ad accounts
+# ads_client.get("accounts")
